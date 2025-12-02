d7d2b25aa4139a0b4ca68484c9bec32bcf1a691b
ltt0716 <2509336485@qq.com>
2025-12-02 17:55:44 +0800

    12-02

--- 二刷/codes/code4/146. LRU 缓存.py
+++ 二刷/codes/code4/146. LRU 缓存.py
@@ -19,32 +19,35 @@ class LRUCache:
 
     def get(self, key: int) -> int:
         if key in self.dir.keys():
-            val,node=self.dir[key]
-            self.movehead(node)
-            return val
+            node=self.dir[key]
+            self.movetohead(node)
+            return node.val
         else:
             return -1
 
     def put(self, key: int, value: int) -> None:
         if key in self.dir.keys():
             
-            _,node=self.dir[key]
+            node=self.dir[key]
             node.val=value
-            self.movehead(node)
+            self.movetohead(node)
+        else:
+            self.addnode(key,value)
 
     
     def addnode(self,key,val):
         node=Node(key,val)
         next=self.dummy_head.next
-
         self.dummy_head.next=node
         node.next=next
         next.pre=node
         node.pre=self.dummy_head
+
         self.size+=1
-        self.dir[key]=(val,node)
+        self.dir[key]=node
         if self.size>self.capacity:
             self.removetail()
+
 
     def movetohead(self,cur):
         pre=cur.pre
@@ -60,7 +63,7 @@ class LRUCache:
 
     
     def removetail(self):
-        self.dir[self.dummy_tail.pre.key]
+        del self.dir[self.dummy_tail.pre.key]
         pre=self.dummy_tail.pre.pre
         pre.next=self.dummy_tail
         self.dummy_tail.pre=pre
