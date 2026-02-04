bb7d60cca0505fb1273038905655de8a065313c6
ltt0716 <2509336485@qq.com>
2026-02-04 11:06:44 +0800

    0204

--- 三刷0203/code1/128. 最长连续序列.py
+++ 三刷0203/code1/128. 最长连续序列.py
@@ -1,3 +1,47 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        
+        # nums.sort()
+
+        # mp=collections.defaultdict(int)
+
+        # for i in nums:
+        #     mp[i]=1
+
+        # n=len(nums)
+
+        # i=0
+
+        # ans=0
+        # count=1
+        # while i<n:
+
+        #     if mp[nums[i]+1]==1:
+        #         count+=1
+        #         i+=1
+        #         while i<n and nums[i]==nums[i-1]:
+        #             i+=1
+
+        #         ans=max(ans,count)
+
+        #     else:
+        #         count=1
+        #         i+=1
+
+        # return ans
+
+        st=set(nums)
+        ans=0
+
+        for num in st:
+            if num-1 in st:
+                continue
+
+            begin=num
+
+            while begin+1 in st:
+                begin+=1
+
+            ans=max(ans,begin-num+1)
+
+        return ans
+            
