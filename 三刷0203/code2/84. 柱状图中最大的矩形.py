c3ec8a11025232bf84ad4f3658023dcd44d42f41
ltt0716 <2509336485@qq.com>
2026-03-19 23:36:11 +0800

    0319

---
+++ 三刷0203/code2/84. 柱状图中最大的矩形.py
@@ -0,0 +1,25 @@
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        n=len(heights)
+
+
+        left=[-1]*n
+        right=[n]*n
+
+        stack=[]
+        for i in range(n):
+            while stack and heights[stack[-1]]>=heights[i]:
+                stack.pop()
+            
+            left[i]=stack[-1] if stack else -1
+            stack.append(i)
+
+        stack=[]
+        for i in range(n-1,-1,-1):
+            while stack and heights[stack[-1]]>=heights[i]:
+                stack.pop()
+            
+            right[i]=stack[-1] if stack else n
+            stack.append(i)
+        
+        return max([heights[i]*(right[i]-left[i]-1) for i in range(n)])
